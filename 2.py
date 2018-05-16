15c28cf39b8a765c66c6dbb9c39519166b4d9345
dpswps12 <dpswps12@daum.net>
2018-05-16 16:11:31 +0900

    first upload

--- 2.py
+++ 2.py
@@ -1,11 +1,11 @@
-str = input("문자열 : ")
+str1 = input("문자열 : ")
 
 print("개별 문자 출력 : ",end="")
-for i in str:
+for i in str1:
     range(len(i))
     print(i,end="")
 print("")
 
 print("역순 개별 문자 출력 : ",end="")
-for j in reversed(str):
+for j in reversed(str1):
     print(j,end="")
